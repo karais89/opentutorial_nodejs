d0444bc89658a2b0af281f004f59b8331740c389
heojihun <karais89@gmail.com>
2016-06-18 11:22:37 +0900

    - Express - 웹페이지를 표현하는 방법

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -5,6 +5,29 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
 	res.send('Hello home page');
 });
+app.get('/dynamic', function (req, res) {
+	var lis = '';
+	for(var i=0; i < 5; i++) {
+		lis = lis + '<li>coding</li>';
+	}
+	var time = Date();
+	var output = `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+		Hello, Dynamic!
+		<ul>
+		${lis}
+		</ul>
+		${time}
+	</body>
+	</html>`;
+	res.send(output);
+});
 app.get('/route', function (req, res) {
 	res.send('Hello Router, <img src="/route.png">')
 })
