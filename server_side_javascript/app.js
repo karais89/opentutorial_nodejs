1ea64ba32feafc522f130932f1a7f5b055fae128
heojihun <karais89@gmail.com>
2016-06-20 21:07:18 +0900

    - express-url을 이용한 정보의 전달

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -4,7 +4,23 @@ app.locals.pretty = true;
 app.set('view engine', 'jade');
 app.set('views', './views');
 app.use(express.static('public'));
-
+app.get('/topic/:id', function(req, res) {
+	var topics = [
+		'Javascript is...',
+		'Nodejs is ...',
+		'Express is ...'
+	];	
+	var output = `
+		<a href="/topic?id=0">JavaScript</a><br>
+		<a href="/topic?id=1">Nodejs</a><br>
+		<a href="/topic?id=2">Express</a><br><br>		
+		${topics[req.params.id]}
+	`
+	res.send(output);
+});
+app.get('/topic/:id/:mode', function(req, res) {
+	res.send(req.params.id + ', ' + req.params.mode);
+});
 app.get('/template', function (req, res) {
 	res.render('temp', {time:Date(), title:'Jade'});
 });
