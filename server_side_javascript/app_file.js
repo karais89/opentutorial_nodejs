3f2b84a2e3c73a077341a059fc4bc7d2919b1198
heojihun <karais89@gmail.com>
2016-06-21 23:31:17 +0900

    본문저장

--- server_side_javascript/app_file.js
+++ server_side_javascript/app_file.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', './views_file');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
@@ -7,7 +10,15 @@ app.get('/topic/new', function(req, res) {
     res.render('new');
 });
 app.post('/topic', function(req, res) {
-    res.send('HI, post');
+    var title = req.body.title;
+    var description = req.body.description;
+    fs.writeFile('data/' + title, description, function(err) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        res.send('Success!');
+    });
 });
 app.listen(3000, function() {
     console.log('Connected, 3000 port!');
