58bfbaf39473f187878a699e44642881443f1b22
jh <jh@2mbgame.com>
2016-06-30 08:46:44 +0900

    - 본문읽기

--- server_side_javascript/app_file.js
+++ server_side_javascript/app_file.js
@@ -19,6 +19,22 @@ app.get('/topic', function (req, res) {
         res.render('view', {topics:files});
     });
 })
+app.get('/topic/:id', function(req, res) {
+    var id = req.params.id;
+    fs.readdir('data', function(err, files) {
+        if(err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+        }
+        fs.readFile('data/' + id, 'utf8', function (err, data) {
+            if(err) {
+                console.log(err);
+                res.status(500).send('Internal Server Error');
+            }
+            res.render('view', {topics:files, title:id, description:data});        
+        });
+    });
+});
 app.post('/topic', function(req, res) {
     var title = req.body.title;
     var description = req.body.description;
