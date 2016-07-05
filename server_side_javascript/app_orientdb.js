f01c6931eb1bd7c84693e7f9bad9c3ad844fdc60
heojihun <karais89@gmail.com>
2016-07-05 21:53:39 +0900

    Orientdb로 웹에플리케이션 제작 12 : 글편집 2

--- server_side_javascript/app_orientdb.js
+++ server_side_javascript/app_orientdb.js
@@ -35,11 +35,6 @@ app.post('/upload', upload.single('userfile'), function(req, res) {
 app.get('/topic/add', function(req, res) {
     var sql = 'SELECT FROM topic';
     db.query(sql).then(function(topics) {
-    // fs.readdir('data', function(err, files) {
-        if(topics.length == 0) {
-            console.log('There is no record');
-            res.status(500).send('Internal Server Error');
-        }
         res.render('add', {topics:topics});
     });
 });
@@ -56,6 +51,33 @@ app.post('/topic/add', function(req, res) {
         }    
     }).then(function(results) {
         res.redirect('/topic/' + encodeURIComponent(results[0]['@rid']));
+    });
+});
+app.get('/topic/:id/edit', function(req, res) {
+    var sql = 'SELECT FROM topic';
+    var id = req.params.id;
+    db.query(sql).then(function(topics) {
+        var sql = 'SELECT FROM topic WHERE @rid=:rid';
+        db.query(sql, {params:{rid:id}}).then(function(topic) {
+            res.render('edit', {topics:topics, topic:topic[0]});
+        });
+    });
+});
+app.post('/topic/:id/add', function(req, res) {
+    var sql = 'UPDATE topic SET title=:t, description=:d, author=:a WHERE @rid=:rid';    
+    var id = req.params.id;
+    var title = req.body.title;
+    var desc = req.body.description;
+    var author = req.body.author;    
+    db.query(sql, {
+        params : {
+            t:title,
+            d:desc,
+            a:author,
+            rid:id
+        }
+    }).then(function(topics) {
+        res.redirect('/topic/'+encodeURIComponent(id));
     });
 });
 app.get(['/topic', '/topic/:id'], function (req, res) {
