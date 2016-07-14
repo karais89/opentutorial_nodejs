62d53efee3d22b07028a48addd7903795194b5c2
jh <jh@2mbgame.com>
2016-07-14 16:52:49 +0900

    Session 4 : login 2

--- server_side_javascript/app_session.js
+++ server_side_javascript/app_session.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var session = require('express-session');
+var bodyParser = require('body-parser');
 var app = express();
 app.use(session({
   secret: 'qgqhgdgwhregsg1234',
   resave: false,
   saveUninitialized: true
 }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/count', function(req, res) {
     if(req.session.count) {
         req.session.count++;
@@ -14,11 +16,25 @@ app.get('/count', function(req, res) {
     }
     res.send('count : ' + req.session.count);
 });
+app.post('/auth/login', function(req, res) {
+    var user = {
+        username:'egoing',
+        password:'111'
+    };
+    var uname = req.body.username;
+    var pwd = req.body.password;
+    if(uname == user.username && pwd == user.password) {
+        res.redirect('/welcome');
+    }else {
+        res.send('Who are you? <a href="/auth/login">login</a>');
+    }    
+});
 app.get('/auth/login', function(req, res) {
     var output = `
+    <h1> Login </h1>
     <form action="/auth/login" method="post">
         <p>
-            <input type="text" name="usernameid" placeholder="username">       
+            <input type="text" name="username" placeholder="username">       
         </p>
         <p>
             <input type="password" name="password" placeholder="password">
@@ -29,9 +45,6 @@ app.get('/auth/login', function(req, res) {
     </form>
     `;    
     res.send(output);
-});
-app.post('/auth/login', function(req, res) {
-
 });
 app.listen(3003, function() {
     console.log('Connected 3003 port !!!');
