09b7ab0a1fb8348469b216047a2d3069e7f0cc2e
heojihun <karais89@gmail.com>
2016-07-18 22:07:10 +0900

    register(회원가입)

---
+++ server_side_javascript/app_multi_user_file.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'qgqhgdgwhregsg1234',
+  resave: false,
+  saveUninitialized: true,
+  store: new FileStore()
+}));
+app.get('/count', function(req, res) {
+    if(req.session.count) {
+        req.session.count++;
+    }else {
+        req.session.count = 1;
+    }
+    res.send('count : ' + req.session.count);
+});
+var users = [
+    {
+        username:'egoing',
+        password:'111',
+        displayName:'Egoing'
+    }
+];
+app.post('/auth/register', function(req, res) {
+    var user = {
+        username:req.body.username,
+        password:req.body.password,
+        displayName:req.body.displayName        
+    };
+    users.push(user);
+    res.send(users);    
+});
+app.get('/auth/register', function(req, res) {
+    var output = `
+    <h1>Register</h1>
+    <form action='/auth/register' method='post'>
+        <p>
+            <input type="text" name="username" placeholder="username">       
+        </p>
+        <p>
+            <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+            <input type="text" name="displayName" placeholder="displayName">
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+    `
+    res.send(output);
+});
+app.get('/auth/logout', function(req, res) {
+    delete req.session.displayName;
+    res.redirect('/welcome');
+});
+app.get('/welcome', function(req, res) {
+    if(req.session.displayName) {
+        res.send(`
+            <h1>Hello, ${req.session.displayName} </h1>
+            <a href="/auth/logout">logout</a>
+        `);        
+    }else {
+        res.send(`
+            <h1>Welcome</h1>
+            <ul>
+                <li><a href='/auth/login'>Login</a></li>
+                <li><a href='/auth/register'>Register</a></li>            
+            </ul>
+        `);
+    }
+});
+app.post('/auth/login', function(req, res) {
+    var user = {
+        username:'egoing',
+        password:'111',
+        displayName:'Egoing'
+    };
+    var uname = req.body.username;
+    var pwd = req.body.password;
+    if(uname == user.username && pwd == user.password) {
+        req.session.displayName = user.displayName;
+        res.redirect('/welcome');
+    }else {
+        res.send('Who are you? <a href="/auth/login">login</a>');
+    }    
+});
+app.get('/auth/login', function(req, res) {
+    var output = `
+    <h1> Login </h1>
+    <form action="/auth/login" method="post">
+        <p>
+            <input type="text" name="username" placeholder="username">       
+        </p>
+        <p>
+            <input type="password" name="password" placeholder="password">
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+    </form>
+    `;    
+    res.send(output);
+});
+app.listen(3003, function() {
+    console.log('Connected 3003 port !!!');
+});
