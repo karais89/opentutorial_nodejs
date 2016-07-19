0b35134c6210b6b4044d04dc67de56674d912309
heojihun <karais89@gmail.com>
2016-07-20 01:09:27 +0900

    - logout

--- server_side_javascript/app_passport_file.js
+++ server_side_javascript/app_passport_file.js
@@ -42,9 +42,11 @@ app.post('/auth/register', function(req, res) {
             displayName:req.body.displayName        
         };
         users.push(user);
-        req.session.displayName = req.body.displayName;
-        req.session.save(function() {      
-            res.redirect('/welcome');      
+        req.login(user, function(err) {
+            req.session.displayName = req.body.displayName;
+            req.session.save(function() {      
+                res.redirect('/welcome');      
+            }); 
         });
     });
 });
@@ -69,13 +71,15 @@ app.get('/auth/register', function(req, res) {
     res.send(output);
 });
 app.get('/auth/logout', function(req, res) {
-    delete req.session.displayName;
-    res.redirect('/welcome');
+    req.logout();
+    req.session.save(function() {
+        res.redirect('/welcome');
+    })
 });
 app.get('/welcome', function(req, res) {
-    if(req.session.displayName) {
+    if(req.user && req.user.displayName) {
         res.send(`
-            <h1>Hello, ${req.session.displayName} </h1>
+            <h1>Hello, ${req.user.displayName} </h1>
             <a href="/auth/logout">logout</a>
         `);        
     }else {
@@ -133,27 +137,6 @@ app.post(
         }
     )
 );
-// app.post('/auth/login', function(req, res) {
-//     console.log('length', users.length);
-//     var uname = req.body.username;
-//     var pwd = req.body.password;
-//     for(var i=0; i<users.length; i++) {
-//         var user = users[i];
-//         if(uname == user.username) {
-//             return hasher({password:pwd, salt:user.salt}, function(err, pass, salt, hash) {
-//                 if(hash == user.password) {
-//                     req.session.displayName = user.displayName;
-//                     req.session.save(function() {
-//                         res.redirect('/welcome'); 
-//                     });                   
-//                 }else {
-//                     res.send('Who are you? <a href="/auth/login">login</a>'); 
-//                 }
-//             });
-//         }
-//     }    
-//     res.send('Who are you? <a href="/auth/login">login</a>');            
-// });
 app.get('/auth/login', function(req, res) {
     var output = `
     <h1> Login </h1>
