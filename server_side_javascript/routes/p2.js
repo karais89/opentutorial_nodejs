ca8316404068ecd872180998000648bafc050a24
heojihun <karais89@gmail.com>
2016-07-24 21:22:15 +0900

    - 라우트 분리하기 3

--- server_side_javascript/routes/p2.js
+++ server_side_javascript/routes/p2.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var route = express.Router();
-route.get('/r1', function(req, res) {
-    res.send('Hello /p2/r1');
-});
-route.get('/r2', function(req, res) {
-    res.send('Hello /p2/r2');
-});
-module.exports = route;
+module.exports = function(app) {
+    var express = require('express');
+    var route = express.Router();
+    route.get('/r1', function(req, res) {
+        res.send('Hello /p2/r1');
+    });
+    route.get('/r2', function(req, res) {
+        res.send('Hello /p2/r2');
+    });    
+    return route;
+}
