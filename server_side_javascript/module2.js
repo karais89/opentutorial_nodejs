f4c240957be9b271b54b0abacd48e41cb07da991
heojihun <karais89@gmail.com>
2016-07-24 20:31:49 +0900

    사용자 정의 모듈 만들기

--- server_side_javascript/module2.js
+++ server_side_javascript/module2.js
@@ -1,2 +1,6 @@
 var sum = require('./lib/sum'); // sum 모듈이 굉장히 복잡하다고 생각해라. 1000줄이 넘는 모듈.
 console.log(sum(1,2));
+
+var cal = require('./lib/calculator');
+console.log('cal sum', cal.sum(1, 2));
+console.log('cal avg', cal.avg(1, 2));
